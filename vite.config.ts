83e726525bacb4208dc8a71c762af2c886aa3684
VOROKZ <159117887+VOROKZ@users.noreply.github.com>
2026-09-16 10:50:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/workpls1337/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
